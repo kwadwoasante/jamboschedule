799fd8081260aaae74716d00646a725a37ae23bf
Kwadwo Asante-Kwabiah <kwadwo@asantekwabiah.com>
2022-07-26 19:45:20 +0000

    testing output

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 
-
-// Define routes here 
-const schedule = require("./routes/schedule");
-
+const { data } = require("./scheduleData");
 
 const app = express();
-
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -18,19 +14,15 @@ app.use(express.urlencoded({ extended : false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
-app.use("/schedule", schedule);
+
+app.get("/", async function(req,res){
+    res.status(200).json({
+        data : data
+    })
+})
 
 app.use(function(req, res, next){
     next(createError(404));
 });
-
-// app.use(function(err,req,res,next){
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get("env") === 'development' ? err : {};
-
-//     res.status(err.status || 500);
-//     res.json({ error: err });
-//     next();
-// });
 
 app.listen(process.env.PORT || 5000);
