bf4a0ce1952147991070b572eab98b25836365de
Kwadwo Asante-Kwabiah <kwadwo@asantekwabiah.com>
2022-07-26 19:23:38 +0000

    updated scheduler output

--- routes/schedule.js
+++ routes/schedule.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const { data } = require("../scheduleData.js");
 
 router.get("/", async function(req,res){
-    // res.status(200).json({
-    //     data : data
-    // })
-    console.log(data);
+    res.status(200).json({
+        data : data
+    })
+    // console.log(data);
 })
 
 module.exports = router;
